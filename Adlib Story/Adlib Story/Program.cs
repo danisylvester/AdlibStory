da0a45d7a5764917049b6ba5c92102f28ad091d3
danisylvester <maria.sylvester10@gmail.com>
2020-08-23 18:42:11 -0500

    Created greeting.

---
+++ Adlib Story/Adlib Story/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Adlib_Story
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("WELCOME TO MADLIBS!");
+            Console.WriteLine("Mad Libs are interactive stories that you add your OWN words to.");
+            Console.WriteLine("Before we begin, what is your name?");
+
+            string name = Console.ReadLine();
+            Console.WriteLine($"It's great to meet you {name}! Now let's begin.");
+
+        }
+    }
+}
